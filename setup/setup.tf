11a70ad60e93d2d29f08b5a644de8f892f91f200
Dan Sali <ruzza.his@gmail.com>
2019-12-16 17:11:11 +1100

    seperate variables into its own file

--- setup/setup.tf
+++ setup/setup.tf
@@ -44,39 +44,3 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     enabled = true
   }
 }
-
-variable "bucket" {
-  type = string
-  description = "Bucket/Dynamodb names"
-}
-
-variable "dynamodb_table" {
-  type = string
-  description = "Bucket/Dynamodb names"
-}
-
-# stop warnings about variables not being used :(
-variable "env" {
-  type = string
-  description = "Environment!"
-}
-
-variable "dbusername" {
-  type = string
-  description = "Database username"
-}
-
-variable "dbpassword" {
-  type = string
-  description = "Database password"
-}
-
-variable "instance_count_min" {
-  type = number
-  description = "Scale instances min"
-}
-
-variable "instance_count_max" {
-  type = number
-  description = "Scale instances max"
-}
