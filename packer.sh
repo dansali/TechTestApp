e3821f2dbf2c86a3f33e6a0fdcedbaa22419a850
Dan Sali <ruzza.his@gmail.com>
2019-12-16 11:15:37 +1100

    Packer validate/encrypt by default

--- packer.sh
+++ packer.sh
@@ -2,6 +2,8 @@
 
 # Freshly builds clean amis
 cd packer
+packer validate centos7-updated-ami.json
+packer validate techtest-app-node-ami.json
 packer build centos7-updated-ami.json
 packer build techtest-app-node-ami.json
 
