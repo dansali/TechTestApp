c577db67d5573041b16fb39fa4b0e47084681905
Dan Sali <ruzza.his@gmail.com>
2019-12-18 17:08:47 +1100

    Packer credentials

--- packer.sh
+++ packer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Freshly builds clean amis
+export AWS_SHARED_CREDENTIALS_FILE=$(pwd)/secret/credentials.ini
+
 cd packer
 packer validate centos7-updated-ami.json
 packer validate techtest-app-node-ami.json
