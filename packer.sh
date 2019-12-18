5755917380fb2699ef43dc9023937b397b9065b0
Dan Sali <ruzza.his@gmail.com>
2019-12-18 17:26:20 +1100

    Add packer ami management install

--- packer.sh
+++ packer.sh
@@ -3,6 +3,15 @@
 # Freshly builds clean amis
 export AWS_SHARED_CREDENTIALS_FILE=$(pwd)/secret/credentials.ini
 
+# if plugin not installed
+if [ ! -f "~/.packer.d/plugins/packer-post-processor-amazon-ami-management" ]; then
+    echo "Installing packer amazon ami management!"
+    mkdir -p ~/.packer.d/plugins
+    wget https://github.com/wata727/packer-post-processor-amazon-ami-management/releases/download/v0.7.0/packer-post-processor-amazon-ami-management_0.7.0_darwin_amd64.zip -P /tmp/
+    cd ~/.packer.d/plugins
+    unzip -j /tmp/packer-post-processor-amazon-ami-management_0.7.0_darwin_amd64.zip -d ~/.packer.d/plugins
+fi
+
 cd packer
 packer validate centos7-updated-ami.json
 packer validate techtest-app-node-ami.json
